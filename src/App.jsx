fd367c4d44a6edf21b7aa336183686048db2c40c
harmeet <159572239+codes-h7@users.noreply.github.com>
2025-09-23 13:14:37 +0530

    Add talwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+
 import Create from './Components/Create'
 import Read from './Components/Read'
 
 const App = () => {
   const [data, setdata] = useState([
-    { id: 1, Tittle: "fds", iscomplete: false }
+    // { id: 1, Tittle: "fds", iscomplete: false }
   ])
   // console.log(data);
 
--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
 const Read = ({ data }) => {
-    let user = data.map(d => <li key={d.id}>{d.Tittle}</li>)
+    let Delete = (id) => {
+        let FilterTodo = data.filter((value) => {
+            value.id != id ? console.log(value.id, value) : "no match"
+            // console.log(value.id);
+
+        })
+        // console.log(FilterTodo);
+
+    }
+    let user = data.map(d => <li key={d.id}>{d.Tittle} | <span className='"bg-sky-500/10' onClick={() => { Delete(d.id) }}>Delete</span></li>)
     return (
         <div>
-            <ul>
+            <ol>
                 {user}
-            </ul>
+            </ol>
         </div>
     )
 }
